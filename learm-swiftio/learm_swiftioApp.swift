0810061cdc23dd6ebc15f8681b6f3ec13886b673
Hafiz Naufal Rahman <hafiz.nr5758@gmail.com>
2021-05-10 21:46:25 +0700

    Initial Commit

---
+++ learm-swiftio/learm_swiftioApp.swift
@@ -0,0 +1,17 @@
+//
+//  learm_swiftioApp.swift
+//  learm-swiftio
+//
+//  Created by Hafiz on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct learm_swiftioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
